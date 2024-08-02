17b26e2f48279005d8d14b68f46be8d42875f22a
Rui Zhi Liew <ruizhi@outlook.my>
2024-08-03 02:23:57 +0800

    feat: lualine as status line

---
+++ nvim/lua/plugins/statusline.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        'nvim-lualine/lualine.nvim',
+        event = "VeryLazy",
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        init = function()
+            vim.g.lualine_laststatus = vim.o.laststatus
+            if vim.fn.argc(-1) > 0 then
+                -- set an empty statusline till lualine loads
+                vim.o.statusline = " "
+            else
+                -- hide the statusline on the starter page
+                vim.o.laststatus = 0
+            end
+
+        end,
+        opts = function()
+            local lualine_require = require("lualine_require")
+            lualine_require.require = require
+
+            vim.o.laststatus = vim.g.lualine_laststatus
+
+        end,
+    }
+}
