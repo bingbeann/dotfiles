3c8fa4fbf003efd344bf8e3b2cd7aa7890e8c52c
Rui Zhi Liew <ruizhi@outlook.my>
2024-08-06 23:10:30 +0800

    feat: telescope grep string with custom string

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,7 +4,13 @@ return {
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
         { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Git Files" },
-        { "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "Find In Files" },
+        {
+            "<leader>fs", 
+            function()
+                require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") });
+
+            end, 
+            desc = "Find In Files" },
     }
 }
 
