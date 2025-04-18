472cbfbf1477694eb0a1ca18bbf9fbd4898e948b
zhi <ruizhi@outlook.my>
2025-04-19 00:56:21 +0800

    feat: useful telescope keybinds

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,15 +2,10 @@ return {
     'nvim-telescope/telescope.nvim', 
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-        { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Git Files" },
-        {
-            "<leader>fs", 
-            function()
-                require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") });
-
-            end, 
-            desc = "Find In Files" },
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "List files in CWD, respects .gitignore" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Searches for a string in CWD, respects .gitignore" },
+        { "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find sorting_strategy=ascending prompt_position=top<cr>", desc = "Search in current buffer" },
+        { "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "List diagnostic(s)" },
     }
 }
 
