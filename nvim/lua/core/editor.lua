0ea66645b0cc55173b928d79dcd78dc3745d4473
bingbeann <ruizhi@outlook.my>
2025-04-21 22:32:36 +0800

    feat: go format on save

--- nvim/lua/core/editor.lua
+++ nvim/lua/core/editor.lua
@@ -16,3 +16,12 @@ vim.opt.autoread = true
 
 vim.opt.colorcolumn = "120"
 
+-- go
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        require('go.format').goimports()
+    end,
+    group = format_sync_grp,
+})
